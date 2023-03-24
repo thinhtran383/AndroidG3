67428c3ee50d42a44db9ac4bf7cd6ea4058e2df5
Thịnh Trần <thinhtran3803@gmail.com>
2023-03-24 11:17:18 +0700

    add titleView

--- flutter_application_1/todo_app/widgets/TitleView.dart
+++ flutter_application_1/todo_app/widgets/TitleView.dart
@@ -1,21 +1,19 @@
 import 'package:flutter/material.dart';
 
 class TitleView extends StatelessWidget{
-  
+  final String contentTitle;
+  const TitleView({Key? key, required this.contentTitle}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-              title: Row(
-                children: [
-                  Icon(Icons.home, color: Colors.black,),
-                  SizedBox(width: 10,),
-                  Text("Home"),
-                ],
-              ),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            );
+    return  Container(
+                        margin: EdgeInsets.only(top: 50, bottom: 20),
+                        child: Text(
+                          contentTitle,
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.w500
+                          ),
+                        ),
+                      );
   }
-
 }
