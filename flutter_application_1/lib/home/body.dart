3ee4d6320a36a08b0f7916ddf005dc7066efbf08
= <=>
2023-04-11 01:41:10 +0700

    hiển thị OKK

--- flutter_application_1/lib/home/body.dart
+++ flutter_application_1/lib/home/body.dart
@@ -9,7 +9,21 @@ import '../model/data/data.dart';
 import '../network/network_request.dart';
 
 class Body extends StatelessWidget {
- final List<String> time = ['6h45','7h40','8h35','9h30','10h25','11h20','12h15','13h10','14h05','15h00','15h55','16h50','17h45'];
+  List<dynamic> time = [
+    '6h45',
+    '7h40',
+    '8h35',
+    '9h30',
+    '10h25',
+    '11h20',
+    '12h15',
+    '13h10',
+    '14h05',
+    '15h00',
+    '15h55',
+    '16h50',
+    '17h45'
+  ];
   final List<Data> data;
 
   Body({Key? key, required this.data}) : super(key: key);
@@ -22,13 +36,96 @@ class Body extends StatelessWidget {
           child: ListView.builder(
             itemCount: data.length,
             itemBuilder: (context, index) {
+              String gioBatDau='';
+              String gioKetThuc='';
+              switch (data[index].TIETBATDAU) {
+                case 1:
+                    gioBatDau = '6h45';
+                   break;
+                   case 2:
+                    gioBatDau = '7h40';
+                   break;
+                   case 3:
+                    gioBatDau = '8h35';
+                   break;
+                   case 4:
+                    gioBatDau = '9h30';
+                   break;
+                   case 5:
+                    gioBatDau = '10h25';
+                   break;
+                   case 6:
+                    gioBatDau = '11h20';
+                   break;
+                   case 7:
+                    gioBatDau = '13h';
+                   break;
+                   case 8:
+                    gioBatDau = '13h55';
+                   break;
+                   case 9:
+                    gioBatDau = '14h50';
+                   break;
+                   case 10:
+                    gioBatDau = '15h45';
+                   break;
+                   case 11:
+                    gioBatDau = '16h40';
+                   break;
+                   case 12:
+                    gioBatDau = '17h35';
+                  break;
+                default:
+              }
+              switch (data[index].TIETKETTHUC) {
+                case 1:
+                    gioKetThuc = '6h45';
+                   break;
+                   case 2:
+                    gioKetThuc = '7h40';
+                   break;
+                   case 3:
+                    gioKetThuc = '8h35';
+                   break;
+                   case 4:
+                    gioKetThuc = '9h30';
+                   break;
+                   case 5:
+                    gioKetThuc = '10h25';
+                   break;
+                   case 6:
+                    gioKetThuc = '11h20';
+                   break;
+                   case 7:
+                    gioKetThuc = '13h';
+                   break;
+                   case 8:
+                    gioKetThuc = '13h55';
+                   break;
+                   case 9:
+                    gioKetThuc = '14h50';
+                   break;
+                   case 10:
+                    gioKetThuc = '15h45';
+                   break;
+                   case 11:
+                    gioKetThuc = '16h40';
+                   break;
+                   case 12:
+                    gioKetThuc = '17h35';
+                  break;
+                default:
+              }
+             
+             
+              
               return Padding(
                 padding: const EdgeInsets.all(12.0),
                 child: ListView(
                   shrinkWrap: true,
                   children: [
-                    for (var index = 0; index < data.length; index++)
-                      Row(
+                    SingleChildScrollView(
+                      child: Row(
                         children: [
                           Expanded(
                             child: Padding(
@@ -53,8 +150,7 @@ class Body extends StatelessWidget {
                                     ), // Hiển thị thông tin thứ
                                     SizedBox(width: 10.0),
                                     Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
+                                      mainAxisAlignment: MainAxisAlignment.center,
                                       crossAxisAlignment:
                                           CrossAxisAlignment.center,
                                       children: [
@@ -71,8 +167,9 @@ class Body extends StatelessWidget {
                                       ],
                                     ),
                                     SizedBox(width: 10.0),
+                    
                                     Text(
-                                      ' ${data[index].TIETBATDAU} \n ${data[index].TIETKETTHUC}',
+                                      '$gioBatDau \n $gioKetThuc',
                                       textAlign: TextAlign.center,
                                       style: TextStyle(
                                         fontSize: 16.0,
@@ -85,6 +182,7 @@ class Body extends StatelessWidget {
                           ),
                         ],
                       ),
+                    ),
                   ],
                 ),
               );
--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -18,8 +18,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    // home: login(),
-      home: home(MSV: '21010627',),
+    home: login(),
+      // home: home(MSV: '21010627',),
       debugShowCheckedModeBanner: false,
     );
   }
@@ -27,29 +27,3 @@ class MyApp extends StatelessWidget {
 
 
 
-// Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text("${widget.MSV}"),
-//         automaticallyImplyLeading: false,
-//       ),
-      
-//       body: Column(
-//         children: [
-//           Expanded(
-//             child: ListView.builder(
-//               itemCount: data.length,
-//               itemBuilder: (context, index) {
-//                 return Card(
-//                   child: ListTile(
-//                     title: Text('${data[index].TENPHONGHOC}'),
-//                     subtitle: Text('${data[index].SOTIET}'),
-//                   ),
-//                 );
-//               },
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
