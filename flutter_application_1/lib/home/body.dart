263cb63b53ec1b31f61fc414c596e236d1862088
Thịnh Trần <thinhtran3803@gmail.com>
2023-04-11 23:53:55 +0700

    reverse

--- flutter_application_1/lib/home/body.dart
+++ flutter_application_1/lib/home/body.dart
@@ -30,108 +30,113 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double displayWidth = MediaQuery.of(context).size.width;
     return Column(
       children: <Widget>[
         Expanded(
           child: ListView.builder(
             itemCount: data.length,
             itemBuilder: (context, index) {
-              String gioBatDau='';
-              String gioKetThuc='';
+              String gioBatDau = '';
+              String gioKetThuc = '';
               switch (data[index].TIETBATDAU) {
                 case 1:
-                    gioBatDau = '6h45';
-                   break;
-                   case 2:
-                    gioBatDau = '7h40';
-                   break;
-                   case 3:
-                    gioBatDau = '8h35';
-                   break;
-                   case 4:
-                    gioBatDau = '9h30';
-                   break;
-                   case 5:
-                    gioBatDau = '10h25';
-                   break;
-                   case 6:
-                    gioBatDau = '11h20';
-                   break;
-                   case 7:
-                    gioBatDau = '13h';
-                   break;
-                   case 8:
-                    gioBatDau = '13h55';
-                   break;
-                   case 9:
-                    gioBatDau = '14h50';
-                   break;
-                   case 10:
-                    gioBatDau = '15h45';
-                   break;
-                   case 11:
-                    gioBatDau = '16h40';
-                   break;
-                   case 12:
-                    gioBatDau = '17h35';
+                  gioBatDau = '6h45';
+                  break;
+                case 2:
+                  gioBatDau = '7h40';
+                  break;
+                case 3:
+                  gioBatDau = '8h35';
+                  break;
+                case 4:
+                  gioBatDau = '9h30';
+                  break;
+                case 5:
+                  gioBatDau = '10h25';
+                  break;
+                case 6:
+                  gioBatDau = '11h20';
+                  break;
+                case 7:
+                  gioBatDau = '13h00';
+                  break;
+                case 8:
+                  gioBatDau = '13h55';
+                  break;
+                case 9:
+                  gioBatDau = '14h50';
+                  break;
+                case 10:
+                  gioBatDau = '15h45';
+                  break;
+                case 11:
+                  gioBatDau = '16h40';
+                  break;
+                case 12:
+                  gioBatDau = '17h35';
                   break;
                 default:
               }
               switch (data[index].TIETKETTHUC) {
                 case 1:
-                    gioKetThuc = '6h45';
-                   break;
-                   case 2:
-                    gioKetThuc = '7h40';
-                   break;
-                   case 3:
-                    gioKetThuc = '8h35';
-                   break;
-                   case 4:
-                    gioKetThuc = '9h30';
-                   break;
-                   case 5:
-                    gioKetThuc = '10h25';
-                   break;
-                   case 6:
-                    gioKetThuc = '11h20';
-                   break;
-                   case 7:
-                    gioKetThuc = '13h';
-                   break;
-                   case 8:
-                    gioKetThuc = '13h55';
-                   break;
-                   case 9:
-                    gioKetThuc = '14h50';
-                   break;
-                   case 10:
-                    gioKetThuc = '15h45';
-                   break;
-                   case 11:
-                    gioKetThuc = '16h40';
-                   break;
-                   case 12:
-                    gioKetThuc = '17h35';
+                  gioKetThuc = '6h45';
+                  break;
+                case 2:
+                  gioKetThuc = '7h40';
+                  break;
+                case 3:
+                  gioKetThuc = '8h35';
+                  break;
+                case 4:
+                  gioKetThuc = '9h30';
+                  break;
+                case 5:
+                  gioKetThuc = '10h25';
+                  break;
+                case 6:
+                  gioKetThuc = '11h20';
+                  break;
+                case 7:
+                  gioKetThuc = '13h00';
+                  break;
+                case 8:
+                  gioKetThuc = '13h55';
+                  break;
+                case 9:
+                  gioKetThuc = '14h50';
+                  break;
+                case 10:
+                  gioKetThuc = '15h45';
+                  break;
+                case 11:
+                  gioKetThuc = '16h40';
+                  break;
+                case 12:
+                  gioKetThuc = '17h35';
                   break;
                 default:
               }
-             
-             
-              
+
               return Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: ListView(
-                  physics: const AlwaysScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  children: [
-                    SingleChildScrollView(
-                      child: Row(
+                padding: const EdgeInsets.fromLTRB(15, 15, 15, 15),
+                child: SingleChildScrollView(
+                   physics: ClampingScrollPhysics(),
+                  child: ListView(
+                   physics: NeverScrollableScrollPhysics(),
+                   
+                   shrinkWrap: true,
+                    children: [
+                      Row(
                         children: [
+                          
                           Expanded(
+                            
                             child: Padding(
+                              
                               padding: const EdgeInsets.only(bottom: 20),
                               child: Container(
+                               
                                 decoration: BoxDecoration(
                                   border: Border.all(color: Colors.grey),
                                   borderRadius: BorderRadius.circular(8.0),
@@ -143,7 +148,7 @@ class Body extends StatelessWidget {
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
                                     Text(
-                                      'THỨ \n ${data[index].THU}',
+                                      '${data[index].THU == '8' ? "CHỦ \n  NHẬT" : "THỨ \n ${data[index].THU}"}',
                                       textAlign: TextAlign.center,
                                       style: TextStyle(
                                         fontSize: 20.0,
@@ -151,7 +156,8 @@ class Body extends StatelessWidget {
                                     ), // Hiển thị thông tin thứ
                                     SizedBox(width: 10.0),
                                     Column(
-                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceEvenly,
                                       crossAxisAlignment:
                                           CrossAxisAlignment.center,
                                       children: [
@@ -159,23 +165,39 @@ class Body extends StatelessWidget {
                                             textAlign: TextAlign.center,
                                             style: TextStyle(
                                               fontSize: 15.0,
+                                              color:
+                                                  Color.fromRGBO(30, 30, 30, 1.0),
                                             )),
                                         Text('${data[index].TENPHONGHOC}',
                                             textAlign: TextAlign.center,
                                             style: TextStyle(
-                                              fontSize: 15.0,
+                                              fontSize: 20.0,
                                             )),
                                       ],
                                     ),
                                     SizedBox(width: 10.0),
-                    
-                                    Text(
-                                      '$gioBatDau \n $gioKetThuc',
-                                      textAlign: TextAlign.center,
-                                      style: TextStyle(
-                                        fontSize: 16.0,
-                                      ),
-                                    ), //
+                
+                                    Column(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        Text('$gioBatDau',
+                                            textAlign: TextAlign.center,
+                                            style: TextStyle(
+                                              color:
+                                                  Color.fromRGBO(30, 30, 30, 1.0),
+                                              fontSize: 17.0,
+                                            )),
+                                        Text('${gioKetThuc}',
+                                            textAlign: TextAlign.center,
+                                            style: TextStyle(
+                                              fontSize: 10.0,
+                                            )),
+                                      ],
+                                    ),
+                
+                                    //
                                   ],
                                 ),
                               ),
@@ -183,8 +205,8 @@ class Body extends StatelessWidget {
                           ),
                         ],
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               );
             },
--- flutter_application_1/lib/home/home.dart
+++ flutter_application_1/lib/home/home.dart
@@ -23,7 +23,7 @@ class _homeState extends State<home> {
   @override
   void initState() {
     super.initState();
-   fetchData('79BE3FF1328F49A397E15EFAF4E83870', '03%2F03%2F2023',
+   fetchData('79BE3FF1328F49A397E15EFAF4E83870', '03%2F04%2F2023',
         '09%2F04%2F2023')
     .then((value) {
   setState(() {
