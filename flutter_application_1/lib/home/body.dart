e5594af723544519e21775a645d807ff7ed2b89c
= <=>
2023-04-12 00:34:00 +0700

    reverse

--- flutter_application_1/lib/home/body.dart
+++ flutter_application_1/lib/home/body.dart
@@ -7,8 +7,6 @@ import 'package:flutter/src/widgets/placeholder.dart';
 
 import '../model/data/data.dart';
 import '../network/network_request.dart';
-import 'canhan.dart';
-import 'homediem.dart';
 
 class Body extends StatelessWidget {
   List<dynamic> time = [
@@ -32,10 +30,6 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double displayWidth = MediaQuery.of(context).size.width;
-   
-
-
     return Column(
       children: <Widget>[
         Expanded(
@@ -124,7 +118,7 @@ class Body extends StatelessWidget {
               }
 
               return Padding(
-                padding: const EdgeInsets.fromLTRB(15, 15, 15, 15),
+                padding: const EdgeInsets.all(12.0),
                 child: SingleChildScrollView(
                    physics: ClampingScrollPhysics(),
                   child: ListView(
@@ -134,14 +128,10 @@ class Body extends StatelessWidget {
                     children: [
                       Row(
                         children: [
-                          
                           Expanded(
-                            
                             child: Padding(
-                              
                               padding: const EdgeInsets.only(bottom: 20),
                               child: Container(
-                               
                                 decoration: BoxDecoration(
                                   border: Border.all(color: Colors.grey),
                                   borderRadius: BorderRadius.circular(8.0),
@@ -169,7 +159,7 @@ class Body extends StatelessWidget {
                                         Text('${data[index].TENHOCPHAN}',
                                             textAlign: TextAlign.center,
                                             style: TextStyle(
-                                              fontSize: 15.0,
+                                              fontSize: 17.0,
                                               color:
                                                   Color.fromRGBO(30, 30, 30, 1.0),
                                             )),
@@ -192,7 +182,7 @@ class Body extends StatelessWidget {
                                             style: TextStyle(
                                               color:
                                                   Color.fromRGBO(30, 30, 30, 1.0),
-                                              fontSize: 17.0,
+                                              fontSize: 20.0,
                                             )),
                                         Text('${gioKetThuc}',
                                             textAlign: TextAlign.center,
