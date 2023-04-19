0923b223b20d8fa29a8d247d786ecbb59c5bd786
= <=>
2023-04-19 15:30:28 +0700

    tách thành các kỳ theo năm, nhưng chưa hiển thị được danh sách tương ứng

--- flutter_application_1/lib/home/diemcanha/diem.dart
+++ flutter_application_1/lib/home/diemcanha/diem.dart
@@ -14,29 +14,60 @@ class diemcanhan extends StatefulWidget {
 }
 
 class _diemcanhanState extends State<diemcanhan> {
-  bool hienthi = false;
-  bool hienthi2 = false;
   @override
   Widget build(BuildContext context) {
-     return Scaffold(
-      appBar: AppBar(
-        title: Text('Danh sách điểm kết thúc học phần'),
-      ),
-      body: ListView.builder(
-        itemCount: widget.data.length,
-        itemBuilder: (BuildContext context, int index) {
-          final item = widget.data[index];
-          return Card(
-            child: ListTile(
-              title: Text(item.DAOTAO_HOCPHAN_TEN),
-              subtitle: Text('Điểm: ${item.DIEM}'),
-              trailing: Text('Điểm quy đổi: ${item.DIEMQUYDOI_TEN}'),
-            ),
+    final Map<String, List<DataDiemKetThucHocPhan>> yearMap = {};
+    for (final item in widget.data) {
+      final key = '${item.NAMHOC}-${item.HOCKY}';
+      if (!yearMap.containsKey(key)) {
+        yearMap[key] = [];
+      }
+      yearMap[key]!.add(item);
+    }
+
+    final List<ExpansionPanel> expansionPanels = yearMap.entries.map((e) {
+      final String yearAndSemester = e.key;
+      final List<DataDiemKetThucHocPhan> dataList = e.value;
+
+      final List<Widget> dataListWidget = dataList.map((data) {
+        return Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: ListTile(
+            
+            title: Text('${data.DAOTAO_HOCPHAN_TEN}'),
+            subtitle: Text('${data.DIEMQUYDOI_TEN}'),
+            trailing: Text('${data.DIEM}'),
+          ),
+        );
+      }).toList();
+
+      final List<String> yearAndSemesterList = yearAndSemester.split('-');
+      final String year = yearAndSemesterList[0];
+      final String semester = yearAndSemesterList[1];
+
+      return ExpansionPanel(
+        headerBuilder: (BuildContext context, bool isExpanded) {
+          return ListTile(
+            title: Text('Năm học $year - Học kỳ $semester'),
           );
         },
+        body: Column(
+          children: dataListWidget,
+        ),
+        isExpanded: false,
+      );
+    }).toList();
+
+    return Scaffold(
+      appBar: null,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(16, 50, 16, 16),
+        child: ExpansionPanelList(
+          
+          expansionCallback: (int index, bool isExpanded) {},
+          children: expansionPanels,
+        ),
       ),
     );
   }
 }
-  
-
